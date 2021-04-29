021608ff7a76689ea80e9898f8ed844496821fba
araiyan <32380335+araiyan@users.noreply.github.com>
2021-04-28 22:58:50 -0700

    Header files created for class

---
+++ Class Nodes With Thread/Class Nodes With Thread/ClassNodes.h
@@ -0,0 +1,27 @@
+#ifndef CLASS_NODES_H
+#define CLASS_NODES_H
+
+class Test
+{
+private:
+    struct Node
+    {
+        double num;
+        Node *nextNode;
+    };
+
+    Node* head = nullptr;
+
+public:
+    Test(double num);
+    void insertNode(double num);
+    void deleteNode(double num);
+    void showList(int index) const;
+    void showAllNodes() const;
+
+    // destructor
+    ~Test();
+
+};
+
+#endif // !CLASS_NODES_HEADER
